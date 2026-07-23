cf31f37cf5e0b9ed361c1f401cc3560cbb11688a
Kyyuu133 <kyyuu@outlook.de>
2026-07-23 10:18:48 +0200

    feat: add s3 remote state backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,8 +7,15 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
 
+backend "s3" {
+    bucket       = "camtrap-sorter-tfstate-636058550437-eu-central-1-an"
+    key          = "terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
 provider "aws" {
   region  = "eu-central-1"
   profile = var.aws_profile
