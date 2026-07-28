cb66bbc3126d3b17adee7644cdacc345ecbf6977
Kyyuu133 <kyyuu@outlook.de>
2026-07-28 09:58:10 +0200

    feat:trigger lambda on s3 object upload

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,3 +14,22 @@ resource "aws_lambda_function" "megadetector_sorter" { # "aws_lambda_function" =
     Purpose = "image-classification"
   }
 }
+
+resource "aws_lambda_permission" "allow_s3" { # erlaubt S3 überhaupt, diese Lambda aufzurufen
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.megadetector_sorter.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raw_images.arn # nur dieser eine Bucket darf die Funktion triggern
+}
+
+resource "aws_s3_bucket_notification" "raw_bucket_trigger" { # legt fest, WANN S3 die Lambda aufruft
+  bucket = aws_s3_bucket.raw_images.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.megadetector_sorter.arn
+    events               = ["s3:ObjectCreated:*"] # löst bei jedem neuen Objekt aus, egal ob Upload/Kopie/Multipart
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3] # Berechtigung muss zuerst existieren, sonst schlägt apply fehl
+}
